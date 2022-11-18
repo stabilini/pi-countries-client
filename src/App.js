5532c5f84f22a9d39b2545eed05d2ffe10587e14
Nicolas Stabilini <stabilini@gmail.com>
2022-11-17 21:21:23 -0300

    fondo terminado

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Route, Switch } from 'react-router-dom';
 
 import LandingPage from './components/LandingPage/LandingPage.jsx';
@@ -6,24 +7,29 @@ import Home from './components/Home/Home.jsx';
 import Detail from './components/Detail/Detail.jsx'
 import CreateActivity from './components/CreateActivity/CreateActivity.jsx'
 
+import styles from './App.module.css';
 import './App.css';
 
 function App() {
+  const theme = useSelector(state => state.theme);
+  
   return (
-    <Switch>
-      <Route exact path='/'>
-        <LandingPage />
-      </Route>
-      <Route exact path='/countries'>
-        <Home />
-      </Route>
-      <Route exact path='/countries/:id'>
-        <Detail />
-      </Route>
-      <Route path='/newactivity'>
-        <CreateActivity />
-      </Route>
-    </Switch>
+    <div className={ `${styles.background} ${styles[theme]}` }>
+      <Switch>
+        <Route exact path='/'>
+          <LandingPage />
+        </Route>
+        <Route exact path='/countries'>
+          <Home />
+        </Route>
+        <Route exact path='/countries/:id'>
+          <Detail />
+        </Route>
+        <Route path='/newactivity'>
+          <CreateActivity />
+        </Route>
+      </Switch>
+    </div>
   );
 }
 
